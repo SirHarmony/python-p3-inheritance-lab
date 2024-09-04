c27a1946c38f0ffbe85ed243416f861f3aa783fa
Harmony <harmonymukolwe@gmail.com>
2024-09-04 15:18:42 +0300

    Completes class inheritance from user to student and teacher subclasses

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,12 @@
 
 from user import User
 
+knowledge = []
+
 class Student(User):
+    def __init__(self,first_name,last_name,knowledge=knowledge):
+        super().__init__(first_name,last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self,knowledge_string):
+        self.knowledge.append(knowledge_string)
